9112c90c34e66b933a8a4e0ebf3efa82e137141c
Gaël Fenet-Garde <gael.fenet.garde@gmail.com>
2015-11-25 09:13:27 +0100

    Initialisation

---
+++ server/README.md
@@ -0,0 +1,7 @@
+# simulateur-flotte-robots-server
+
+## Présentation
+
+Le serveur java du projet
+
+## Règles d'ajout/modification de classe
---
+++ server/src/main/java/fr/univ/tlse2/sfr/server/App.java
@@ -0,0 +1,13 @@
+package fr.univ.tlse2.sfr.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
