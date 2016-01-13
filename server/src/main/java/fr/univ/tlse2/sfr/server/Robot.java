50e86136d87110769dca2165c67b858303b0987f
Yannick Atchy-Dalama <yannick.atchy.dalama@gmail.com>
2016-01-13 10:39:20 +0100

    orientation + deplacement +

--- server/src/main/java/fr/univ/tlse2/sfr/server/Robot.java
+++ server/src/main/java/fr/univ/tlse2/sfr/server/Robot.java
@@ -1,12 +1,17 @@
 package fr.univ.tlse2.sfr.server;
 
+import java.util.Random;
+
 import fr.univ.tlse2.sfr.communication.Position;
 
 public class Robot {
 	private int id_robot;
 	private Position pos_robot;
+	/** orientation en degres*/
 	private int orientation_robot;
 	private double vitesse;
+	private Random random_orientation;
+	private int coef_orientation;
 	
 	//constructeur par défaut
 	public Robot(){
@@ -48,14 +53,26 @@ public class Robot {
 		double futur_x;
 		double futur_y; 
 		
-		futur_x = this.pos_robot.get_x();
-		futur_y = this.pos_robot.get_y(); 		
+		futur_x = this.pos_robot.get_x() + Math.cos(Math.toRadians(orientation_robot)) * vitesse;
+		futur_y = this.pos_robot.get_y() + Math.sin(Math.toRadians(orientation_robot)) * vitesse;
+		this.pos_robot.set_x(futur_x);
+		this.pos_robot.set_y(futur_y);
 		
 	}
 
 	private void determiner_nouvelle_orientation() {
-		// TODO Auto-generated method stub
-		
+		// on determine la nouvelle orientation
+		this.random_orientation = new Random();
+		this.coef_orientation = random_orientation.nextInt(101);
+		//boolean coef_orientationisbetween = 
+		if (0 <= this.coef_orientation && this.coef_orientation <= 5)
+			this.orientation_robot = (orientation_robot - 60) % 360;
+		else if (6 <= this.coef_orientation && this.coef_orientation <= 15)
+			this.orientation_robot = (orientation_robot - 30) % 360;
+		else if (86 <= this.coef_orientation && this.coef_orientation <= 95)
+			this.orientation_robot = (orientation_robot + 30) % 360;
+		else if (96 <= this.coef_orientation && this.coef_orientation <= 100)
+			this.orientation_robot = (orientation_robot + 60) % 360;
 	}
 	
 	
