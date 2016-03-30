09f0b09ef9d1217db3d35f86555054dcd9a6f889
GaelFG <gael.fenet.garde@gmail.com>
2016-03-30 11:44:14 +0200

    Modif commnde arret serveur

--- server/src/main/java/fr/univ/tlse2/sfr/server/EcouteurReseau.java
+++ server/src/main/java/fr/univ/tlse2/sfr/server/EcouteurReseau.java
@@ -3,6 +3,7 @@ package fr.univ.tlse2.sfr.server;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
+import fr.univ.tlse2.sfr.communication.ArreterSimulation;
 import fr.univ.tlse2.sfr.communication.DemarrerSimulation;
 import fr.univ.tlse2.sfr.communication.MessageTexte;
 
@@ -19,18 +20,17 @@ public class EcouteurReseau extends Listener{
          System.out.println("le serveur recoit qqch");
 		 if (object instanceof DemarrerSimulation) {
        	  DemarrerSimulation demande_demarrage_simulation = (DemarrerSimulation)object;
-            System.out.println("Demande de d�marrage de simuation appell�e " + demande_demarrage_simulation.nom_simulation);
-            MessageTexte reponse = new MessageTexte("Demande de d�marrage de simulation recu !");
+            System.out.println("Demande de démarrage de simuation appellée " + demande_demarrage_simulation.nom_simulation);
+            MessageTexte reponse = new MessageTexte("Demande de démarrage de simulation recu !");
             connection.sendTCP(reponse);
             programme_serveur.set_etat_simulation(true);
             programme_serveur.run();
          }
 		 else if (object instanceof MessageTexte) {
-			 System.out.println(((MessageTexte) object).get_contenu());
 			 String message = ((MessageTexte)object).get_contenu();
-			 if (message.equals("STOP")) {
-				 programme_serveur.set_etat_simulation(false);
-			 }			 
+			 System.out.println(message);
+		 } else if (object instanceof ArreterSimulation) {
+			 programme_serveur.set_etat_simulation(false);
 		 } else {
 			 System.err.println("commande inconnue");
 		 }
