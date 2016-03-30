d09ad50066354cb9fefd08b2dc6ccd59f1ffde6c
Fantinel Lucas <lucas.fantinel@gmail.com>
2016-03-30 09:47:03 +0200

    fonction play

--- server/src/main/java/fr/univ/tlse2/sfr/server/EcouteurReseau.java
+++ server/src/main/java/fr/univ/tlse2/sfr/server/EcouteurReseau.java
@@ -7,20 +7,35 @@ import fr.univ.tlse2.sfr.communication.DemarrerSimulation;
 import fr.univ.tlse2.sfr.communication.MessageTexte;
 
 /**
- * L'Ã©couteur rÃ©seau du serveur. DÃ©finit la rÃ©action du serveur aux messages envoyÃ©s.
+ * L'écouteur réseau du serveur. Définit la réaction du serveur aux messages envoyés.
  */
 public class EcouteurReseau extends Listener{
-	
-	/** RÃ©agit Ã  la reception d'un objet sÃ©rialisÃ©. */
+	private ProgrammeServeur programme_serveur;
+	public EcouteurReseau (ProgrammeServeur programme_serveur) {
+		this.programme_serveur = programme_serveur;
+	}
+	/** Réagit à la reception d'un objet sérialisé. */
 	 public void received (Connection connection, Object object) {
-         
+         System.out.println("le serveur recoit qqch");
 		 if (object instanceof DemarrerSimulation) {
        	  DemarrerSimulation demande_demarrage_simulation = (DemarrerSimulation)object;
-            System.out.println("Demande de dÃ©marrage de simuation appellÃ©e " + demande_demarrage_simulation.nom_simulation);
-            MessageTexte reponse = new MessageTexte("Demande de dÃ©marrage de simulation recu !");
+            System.out.println("Demande de démarrage de simuation appellée " + demande_demarrage_simulation.nom_simulation);
+            MessageTexte reponse = new MessageTexte("Demande de démarrage de simulation recu !");
             connection.sendTCP(reponse);
+            programme_serveur.set_etat_simulation(true);
+            programme_serveur.run();
          }
-		 
-		 
+		 else if (object instanceof MessageTexte) {
+			 System.out.println(((MessageTexte) object).get_contenu());
+			 String message = ((MessageTexte)object).get_contenu();
+			 if (message.equals("STOP"))
+			 {
+				 programme_serveur.set_etat_simulation(false);
+			 }
+		 }
+		 else
+		 {
+			 System.err.println("commande inconnue");
+		 }
       }
 }
